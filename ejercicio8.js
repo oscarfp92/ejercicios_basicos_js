6dc6b8e0d289bf530a8e52978fcfb72ddcbf338c
Oscarfp <oscarfp1992@gmail.com>
2024-07-17 18:13:37 +0200

    ok

---
+++ ejercicio8.js
@@ -0,0 +1,11 @@
+const avengers = ['Hulk', 'Thor', 'Iron Man', 'Captain A.', 'Spiderman', 'Captain M.'];
+function findLongestWord(stringlist) {
+let longest= avengers[0];
+for(i = 0; i < avengers.length ; i++){
+  if(avengers[i].length>longest.length){
+    longest=avengers[i];
+  }
+}
+return longest
+}
+console.log(findLongestWord(avengers))
