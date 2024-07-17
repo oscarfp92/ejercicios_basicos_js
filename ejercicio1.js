f5c87cd916ff2cdcaa835b47a29fd3d5c3f6a680
Oscarfp <oscarfp1992@gmail.com>
2024-07-17 10:31:41 +0200

    ok

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,8 +1,8 @@
-miSuperheroeFavorito="Hulk"
-numeroFavorito="50"
-height=5
-length=10
-suma=(height+length)
+let miSuperheroeFavorito="Hulk"
+let numeroFavorito="50"
+let altura=5
+let longitud=10
+let suma=(altura+longitud)
 console.log (miSuperheroeFavorito)
 console.log (numeroFavorito)
 console.log (suma)
