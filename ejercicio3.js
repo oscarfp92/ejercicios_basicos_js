1035d49511b6b92cf7f31f8f1e191287e8aff73e
Oscarfp <oscarfp1992@gmail.com>
2024-07-19 12:44:20 +0200

    repaso

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,23 +1,24 @@
-// 3.1 Multiplica 10 por 5 y muestra el resultado mediante console.
-let suma=10*5
-console.log(suma)
+//1. Miltuplica 10 por 5 y muestra el resultado mediante console.
+console.log (10*5);
 
-// 3.2 Divide 10 por 2 y muestra el resultado en un console.
-let division=10/2
-console.log(division)
+//2. Divide 10 por 2 y muestra el resultado en un console.
+console.log (10/2);
 
-// 3.3 Muestra mediante un console el resto de dividir 15 por 9.
-let resto=15%9
-console.log(resto)
+//3. Muestra mediante un console el resto de dividir 15 por 9.
+console.log (15%9);
 
-// 3.4 Usa el correcto operador de asignación que resultará en o = 15, teniendo dos variables p = 10 y j = 5.
-let p=10
-let j=5
-let o=(p+j)
-console.log(o)
+//4. Usa el correcto operador de asignación que resultará en o = 15, teniendo dos variables p = 10 y j = 5.
+let p = 10;
+let j = 5;
+let o = p;
+o += j;
 
-// 3.5 Usa el correcto operador de asignación que resultará en i = 50, teniendo dos variables c = 10 y m = 5.
-let c=10
-let m=5
-let i=(c*m)
-console.log(i)
+console.log(o);
+
+//5. Usa el correcto operador de asignación que resultará en i = 50, teniendo dos variables c = 10 y m = 5.
+let c = 10;
+let m = 5;
+i = c;
+i *= m;
+
+console.log (i);
