a195ed2bd0de2c611458594624b039ffdc0ebd27
Oscarfp <oscarfp1992@gmail.com>
2024-07-22 13:10:12 +0200

    repaso

--- ejercicio10.js
+++ ejercicio10.js
@@ -1,10 +1,9 @@
 const numbers = [12, 21, 38, 5, 45, 37, 6];
 function average(numberList) {
-  let result=0
-  for(let i = 0; i < numberList.length; i++){
-  result += numberList[i]
-  };
-  const media = result / numberList.length;
-  return media;
+  media = 0;
+  for (i = 0; i < numbers.length; i++) {
+    media += numbers[i] / numbers.length
+} 
+return media;
 }
-console.log (average(numbers))
+console.log (average (numbers))
